7bc2da52eb8b40870272a007f785b70aaa7556c7
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2021-03-06 19:36:08 +0900

    現在の桁数が１つ多かったので修正

--- js/pi.js
+++ js/pi.js
@@ -2,6 +2,7 @@ var pi_flag = false;
 var pi_time = 0;
 var pi_digit = 0;
 var pi_index = 0;
+var pi_now = 0;
 var pi_suuji_ima = document.getElementById("pi_suuji_ima");
 var pi_nokori = document.getElementById("pi_nokori");
 var pi_suuji_mae = document.getElementById("pi_suuji_mae");
@@ -20,7 +21,7 @@ function op_beatmap(data) {
 		pi_digit = 10238;
 		pi_index = 0;
 		//pi_nokori.innerText = pi_digit.toString();
-		pi_ima.innerText = note_list[0].toString() + "桁";
+		pi_ima.innerText = "0桁";
 		pi_nokori.innerText = "(あと" + pi_digit.toString() + "桁)";
 		pi_suuji_mae.innerText = pi_list.charAt(pi_index - 10);
 		pi_suuji_mae.innerText += pi_list.charAt(pi_index - 9);
@@ -55,29 +56,34 @@ function op_beatmap(data) {
 function pi(noteCut) {
 	if (pi_flag) {
 		pi_index = note_list[noteCut.noteID]
-		if (pi_index > 1) pi_digit = 10238 - note_list[noteCut.noteID] + 1;
-			pi_suuji_mae.innerText = pi_list.charAt(pi_index - 10);
-			pi_suuji_mae.innerText += pi_list.charAt(pi_index - 9);
-			pi_suuji_mae.innerText += pi_list.charAt(pi_index - 8);
-			pi_suuji_mae.innerText += pi_list.charAt(pi_index - 7);
-			pi_suuji_mae.innerText += pi_list.charAt(pi_index - 6);
-			pi_suuji_mae.innerText += pi_list.charAt(pi_index - 5);
-			pi_suuji_mae.innerText += pi_list.charAt(pi_index - 4);
-			pi_suuji_mae.innerText += pi_list.charAt(pi_index - 3);
-			pi_suuji_mae.innerText += pi_list.charAt(pi_index - 2);
-			pi_suuji_mae.innerText += pi_list.charAt(pi_index - 1);
-			pi_suuji_ima.innerText = pi_list.charAt(pi_index);
-			pi_suuji_ushiro.innerText = pi_list.charAt(pi_index + 1);
-			pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 2);
-			pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 3);
-			pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 4);
-			pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 5);
-			pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 6);
-			pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 7);
-			pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 8);
-			pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 9);
-			pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 10);
-		pi_ima.innerText = note_list[noteCut.noteID].toString() + "桁";
+		pi_digit = 10238;
+		pi_now = 0;
+		if (pi_index > 1) {
+			pi_digit = 10238 - pi_index + 1;
+			pi_now = pi_index - 1;
+		}
+		pi_suuji_mae.innerText = pi_list.charAt(pi_index - 10);
+		pi_suuji_mae.innerText += pi_list.charAt(pi_index - 9);
+		pi_suuji_mae.innerText += pi_list.charAt(pi_index - 8);
+		pi_suuji_mae.innerText += pi_list.charAt(pi_index - 7);
+		pi_suuji_mae.innerText += pi_list.charAt(pi_index - 6);
+		pi_suuji_mae.innerText += pi_list.charAt(pi_index - 5);
+		pi_suuji_mae.innerText += pi_list.charAt(pi_index - 4);
+		pi_suuji_mae.innerText += pi_list.charAt(pi_index - 3);
+		pi_suuji_mae.innerText += pi_list.charAt(pi_index - 2);
+		pi_suuji_mae.innerText += pi_list.charAt(pi_index - 1);
+		pi_suuji_ima.innerText = pi_list.charAt(pi_index);
+		pi_suuji_ushiro.innerText = pi_list.charAt(pi_index + 1);
+		pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 2);
+		pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 3);
+		pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 4);
+		pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 5);
+		pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 6);
+		pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 7);
+		pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 8);
+		pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 9);
+		pi_suuji_ushiro.innerText += pi_list.charAt(pi_index + 10);
+		pi_ima.innerText = pi_now.toString() + "桁";
 		pi_nokori.innerText = "(あと" + pi_digit.toString() + "桁)";
 	}
 }
